24c717baefa25ddf8a4d81e3aa72c565a7cd91cb
MarcosBernal <marcosbernal3@hotmail.com>
2019-04-09 01:52:30 +0200

    Add interactive script and commands to compile from terminal

---
+++ README.md
@@ -0,0 +1,46 @@
+## Requirements
+
+- Java 8
+- Docker (or workbench application) 
+
+## Environment
+
+Launch environment:
+
+Mysql server:
+
+    docker run -d --rm -e MYSQL_ROOT_PASSWORD=secret -p 3306:3306 mysql
+        
+Mysql client:
+    
+    pip install --user mycli
+    mycli -u root -h localhost -psecret
+
+## Compile exercises
+
+With the ide (skip this step) or with the terminal:
+
+    mkdir build
+    
+    # Compile the java files and place the class object into a similar structure in the build folder
+    javac -d ./build -cp mysql-connector-java-8.0.15.jar src/exercise*/*.java src/AssignmentSolver.java
+
+
+## Execution
+
+Using an IDE, tool such as maven. 
+
+With the classpath:
+
+    cd ..
+    java -cp mysql-connector-java-8.0.15.jar:build/ AssignmentSolver localhost secret cataVino
+
+
+## Exercises
+
+#### Exercise 1
+...
+#### Exercise 2
+...
+#### Exercise 3
+...
---
+++ src/AssignmentSolver.java
@@ -0,0 +1,63 @@
+import exercise3.JDBCConnector;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class AssignmentSolver {
+
+    public static void main(String[] args) throws SQLException {
+        HashMap<String, String> expected_args = argscheck_args(args);
+        int number = -1;
+        while(number != 0) {
+            System.out.println("Insert the exercise that you want to do (1-9 and 0 is exit)");
+            try {
+                number = Integer.parseInt(System.console().readLine());
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Only number are allowed!!! ");
+            }
+            if (number > 0 && number < 10)
+                do_exercise(number, expected_args);
+
+            number = -0;
+        }
+    }
+
+    public static void do_exercise(int number, HashMap<String, String> expected_args) throws SQLException {
+        switch(number){
+            case 1:
+            case 2: System.out.println("No done yet D:");
+                    break;
+            case 3:
+                JDBCConnector myJdbc = new JDBCConnector(expected_args.get("host"), expected_args.get("password"), expected_args.get("database"));
+                ResultSet result3 = myJdbc.executeQuery("SELECT count(*) from user");
+                while(result3.next())
+                    System.out.println("There are " + result3.getInt("count(*)") + " users");
+                result3.close();
+                System.out.println("Exercise 3 complete!!");
+                break;
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9: System.out.println("No done yet D:");
+                    break;
+
+        }
+    }
+
+    public static HashMap argscheck_args(String[] args){
+        if(args.length != 3){
+            System.out.println("Require 3 parameters: host, password and database\nTry again!! Closing...");
+            System.exit(1);
+        }
+        HashMap<String,String> expected_args = new HashMap();
+        expected_args.put("host", args[0]);
+        expected_args.put("password", args[1]);
+        expected_args.put("database", args[2]);
+        return expected_args;
+    }
+}
